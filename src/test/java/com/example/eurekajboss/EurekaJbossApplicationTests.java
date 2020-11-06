66929e37147a447379a73f3ff0933c529a10925f
phipop <phipop.k@gmail.com>
2020-11-07 00:20:12 +0700

    first commit

---
+++ src/test/java/com/example/eurekajboss/EurekaJbossApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eurekajboss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaJbossApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
